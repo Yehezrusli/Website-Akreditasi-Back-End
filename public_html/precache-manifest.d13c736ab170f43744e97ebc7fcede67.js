fb9e43bb915c7007ee811ba98a72d0be7cbda878
Chris <christianto.g.14@gmail.com>
2019-12-06 01:31:02 +0700

    add react build

---
+++ public_html/precache-manifest.d13c736ab170f43744e97ebc7fcede67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51855b1dfb3c9a1fe4eb7f1e6056b64f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deba039274f633e40177",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8e8ee70b580dabb0db86",
+    "url": "/static/css/main.5c2201d6.chunk.css"
+  },
+  {
+    "revision": "deba039274f633e40177",
+    "url": "/static/js/2.9353d8d9.chunk.js"
+  },
+  {
+    "revision": "8e8ee70b580dabb0db86",
+    "url": "/static/js/main.599c8ca7.chunk.js"
+  },
+  {
+    "revision": "1b651740a5504f08a6f7",
+    "url": "/static/js/runtime~main.00905a2d.js"
+  },
+  {
+    "revision": "7bba1772bd579da0fdcaad80b89eff01",
+    "url": "/static/media/Logo-Informatika.7bba1772.png"
+  }
+]);
