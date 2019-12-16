8380d628ce6cc825e26ea539a04fdc3c04f830e1
Chris <christianto.g.14@gmail.com>
2019-12-16 13:20:30 +0700

    update 2

--- public_html/service-worker.js
+++ public_html/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5aa5016750a0e724eb92936d8dd22f4.js"
+  "/precache-manifest.e24502c1f1d071d9c4c492186904c7dc.js"
 );
 
 self.addEventListener('message', (event) => {
