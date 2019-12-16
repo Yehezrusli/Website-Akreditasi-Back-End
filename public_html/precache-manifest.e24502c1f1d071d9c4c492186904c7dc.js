7ac3d3fc25298a453986b64510fca4ed38413d96
Chris <christianto.g.14@gmail.com>
2019-12-16 13:21:10 +0700

    update 3

---
+++ public_html/precache-manifest.e24502c1f1d071d9c4c492186904c7dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77868afb87b63336245e8517fd7887c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5feadda46216880e3660",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1028b44f6f56d10f5d26",
+    "url": "/static/css/main.66d328e2.chunk.css"
+  },
+  {
+    "revision": "5feadda46216880e3660",
+    "url": "/static/js/2.93938ef1.chunk.js"
+  },
+  {
+    "revision": "1028b44f6f56d10f5d26",
+    "url": "/static/js/main.5f76aab9.chunk.js"
+  },
+  {
+    "revision": "1b651740a5504f08a6f7",
+    "url": "/static/js/runtime~main.00905a2d.js"
+  },
+  {
+    "revision": "7bba1772bd579da0fdcaad80b89eff01",
+    "url": "/static/media/Logo-Informatika.7bba1772.png"
+  },
+  {
+    "revision": "2f7da980c068a412337820de58469d35",
+    "url": "/static/media/UNPAR.2f7da980.png"
+  },
+  {
+    "revision": "10252707b6ee2ed31a73930853cc6db0",
+    "url": "/static/media/tulisan.10252707.png"
+  }
+]);
